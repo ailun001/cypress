cf9278f09d751ea367e2fac58c664f97e7461511
ailun001 <43659626+ailun001@users.noreply.github.com>
2022-08-04 14:21:33 +1200

    Arrow function expressions
    // function () {}
    () => {  }
    () => return

--- cypress/cypress/e2e/sample/sample.cy.js
+++ cypress/cypress/e2e/sample/sample.cy.js
@@ -1,13 +1,9 @@
 /// <reference types="cypress" />
 
-describe("class1 test", function () {
-  it("fail test 10 + 15 => 15", function () {
-    expect(10 + 15).to.equal(15);
-  });
-  it("pass test 10 + 15 => 25", function () {
-    expect(10 + 15).to.equal(25);
-  });
-  it("some cy use", function () {
+describe("class1 test", () => {
+  it("fail test 10 + 15 => 15", () => expect(10 + 15).to.equal(15));
+  it("pass test 10 + 15 => 25", () => expect(10 + 15).to.equal(25));
+  it("some cy use", () => {
     cy.visit("https://www.google.com");
 
     cy.get(".SDkEP").click().type("cypress");
