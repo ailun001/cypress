0805ecb30461a490d7d7cce1461d6a2456392eb3
ailun001 <43659626+ailun001@users.noreply.github.com>
2022-08-07 01:54:23 +1200

    Revert "Arrow function expressions"
    This reverts commit cf9278f09d751ea367e2fac58c664f97e7461511.

--- cypress/cypress/e2e/sample/sample.cy.js
+++ cypress/cypress/e2e/sample/sample.cy.js
@@ -1,9 +1,13 @@
 /// <reference types="cypress" />
 
-describe("class1 test", () => {
-  it("fail test 10 + 15 => 15", () => expect(10 + 15).to.equal(15));
-  it("pass test 10 + 15 => 25", () => expect(10 + 15).to.equal(25));
-  it("some cy use", () => {
+describe("class1 test", function () {
+  it("fail test 10 + 15 => 15", function () {
+    expect(10 + 15).to.equal(15);
+  });
+  it("pass test 10 + 15 => 25", function () {
+    expect(10 + 15).to.equal(25);
+  });
+  it("some cy use", function () {
     cy.visit("https://www.google.com");
 
     cy.get(".SDkEP").click().type("cypress");
