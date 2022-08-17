5e15b9d0d7a5f83f665ab0c0e1c5852586ceb155
ailun001 <43659626+ailun001@users.noreply.github.com>
2022-08-18 02:39:49 +1200

    course changes
    login command add
    wait before input name (css json respond)
    url command add
    remove clear date check

--- cypress/cypress/e2e/sample/manager.courses.cy.js
+++ cypress/cypress/e2e/sample/manager.courses.cy.js
@@ -2,33 +2,22 @@
 
 describe("header of homepage", () => {
   beforeEach(function () {
-    cy.visit("/login");
-    cy.get("#login_role > :nth-child(3)").click();
-    cy.get("#login_email").type("manager@admin.com");
-    cy.get("#login_password").type("111111");
-    cy.get("[type=submit]").click();
-    cy.url().should("contain", "dashboard/manager");
-    //login (import)
+    cy.login('manager@admin.com','manager');
   });
 
   it('add courses', () =>{
     cy.get(':nth-child(4) > .ant-menu-submenu-title').click();
     cy.get('[title="Add Course"]').click();
-    cy.intercept('GET','https://code.highcharts.com/mapdata/custom/world-palestine-highres.geo.json').as('toAdd');
-    cy.wait('@toAdd');
 
-    cy.get('#name').type('aaa');
+    cy.get('#name',{ timeout:10000 }).type('aaa');
     cy.get('#teacherId').type('aa');
-    cy.intercept('GET','http://cms.chtoma.com/api/teachers?query=aa').as('teacherId');
-    //cy.wait('@teacherId');
+    cy.intercept('GET',cy.apiUrl()+'/teachers?query=aa').as('teacherId');
     cy.contains('Fanny Haag').click();
     
-    //cy.get('#type').focus();
     cy.get('#type').click();
-    cy.intercept('GET','http://cms.chtoma.com/api/courses/type').as('type')
+    cy.intercept('GET',cy.apiUrl()+'/courses/type').as('type')
     //cy.get('.rc-virtual-list-holder-inner').scrollTo('bottom');
     //cy.get('.ant-select-item-option').contains('aaa').click();
-    //select? scrolldown? focus()?
     cy.get('.ant-select-item-option').contains('C').click();
     cy.get('.ant-select-item-option').contains('C++').click();
     cy.get('.ant-select-item-option').contains('C+').click();
@@ -39,9 +28,6 @@ describe("header of homepage", () => {
     const timeNow = dayjs().format('YYYY-MM-DD');
     const timeTmr = dayjs().add(1,'day').format('YYYY-MM-DD');
     cy.get(`[title=${timeNow}]`).should('be.visible');
-    //cy.get('.ant-picker-clear').focus();
-    //cy.get('.ant-picker-clear > .anticon > svg').should('be.visible').click();
-    //cy.get('.ant-picker-suffix').click();
     cy.get('#startTime').click();
     cy.get(`td[title=${timeTmr}]`).click();
 
@@ -56,7 +42,6 @@ describe("header of homepage", () => {
     cy.get('#detail').type('e2e abc example need have 100 word x xxxxx xxxxx xxxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx.103');
 
     cy.get('[type="submit"]').contains('Create').click();
-
-    cy.intercept('POST','http://cms.chtoma.com/api/courses').as('detail');
+    cy.intercept('POST',cy.apiUrl()+'/courses').as('detail');
 })
 });
